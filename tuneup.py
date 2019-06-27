7191c67e18559f26f9478cfba8ad61ed1e6960e4
Survi <suravitaroy@gmail.com>
2019-06-26 23:40:21 -0400

    optimized tuneup.py

--- tuneup.py
+++ tuneup.py
@@ -2,11 +2,13 @@
 # -*- coding: utf-8 -*-
 """Tuneup assignment"""
 
-__author__ = "???"
+__author__ = "jupiter2018"
 
 import cProfile
 import pstats
 import functools
+import timeit
+import StringIO
 
 
 def profile(func):
@@ -14,7 +16,24 @@ def profile(func):
     # You need to understand how decorators are constructed and used.
     # Be sure to review the lesson material on decorators, they are used
     # extensively in Django and Flask.
-    raise NotImplementedError("Complete this decorator function")
+    def inner_wrapper(*args, **kwargs):
+        # Do something before calling func_to_decorate
+        pr = cProfile.Profile()
+        pr.enable()
+        result = func(*args, **kwargs)
+        pr.disable()
+        s = StringIO.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        # Do something after calling func_to_decorate
+        return result
+
+    return inner_wrapper
+
+
+"""raise NotImplementedError("Complete this decorator function")"""
 
 
 def read_movies(src):
@@ -27,25 +46,33 @@ def read_movies(src):
 def is_duplicate(title, movies):
     """returns True if title is within movies list"""
     for movie in movies:
-        if movie.lower() == title.lower():
+        if movie == title:
             return True
     return False
 
 
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
-    duplicates = []
-    while movies:
-        movie = movies.pop()
-        if is_duplicate(movie, movies):
-            duplicates.append(movie)
+    movies = [movie.lower() for movie in movies]
+    movies.sort()
+    duplicates = [movie1 for movie1, movie2 in zip(movies[:-1], movies[1:])
+                  if movie1 == movie2]
+#     duplicates = []
+#     while movies:
+#         movie = movies.pop()
+#         if movie in movies:
+#             duplicates.append(movie)
     return duplicates
 
 
 def timeit_helper():
     """Part A:  Obtain some profiling measurements using timeit"""
     # YOUR CODE GOES HERE
+    t = timeit.Timer(main)
+    result = t.repeat(repeat=7, number=3)
+    return result
 
 
 def main():
@@ -57,3 +84,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # print(timeit_helper())
